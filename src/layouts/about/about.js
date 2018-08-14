f326ae0426f311ac3d29e7390eecc0fa8684ee03
boxton90 <daniel.rabal@dnata.com>
2018-08-14 08:48:44 +0400

    About layout component improvements - Custom stollbar

--- src/layouts/about/about.js
+++ src/layouts/about/about.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import avatar from "../../assets/avatar.jpg"
+
+console.log(avatar);
 
 class About extends Component {
   render() {
     return (
-      <section className="section">
+      <section className="section has-text-centered">
         <div className="container">
           <h1 className="title">About me</h1>
+          <figure className="image is-128x128">
+            <img className="is-rounded" src={avatar}></img>
+          </figure>
+          <p>
+            American bobtail tiger but bengal sphynx egyptian mau or burmese lion.
+                Devonshire rex. Puma ragdoll, and mouser yet panther bombay yet malkin for panther.
+                Kitten. Kitten norwegian forest for tom for grimalkin so puma so siberian.
+                Russian blue russian blue scottish fold for kitten. Devonshire rex grimalkin. Cheetah kitten.
+                Jaguar tom for russian blue lynx. Puma cornish rex so egyptian mau and jaguar so mouser.
+                Havana brown thai for devonshire rex birman yet bobcat or balinese.
+          </p>
         </div>
       </section>
     );
