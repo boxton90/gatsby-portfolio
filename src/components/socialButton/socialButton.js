c4a6e72bc74e6ab186d9e4346df42a5ca1bc4eb6
boxton90 <daniel.rabal@dnata.com>
2018-09-04 11:28:50 +0400

    Socia button improvements

--- src/components/socialButton/socialButton.js
+++ src/components/socialButton/socialButton.js
@@ -5,6 +5,6 @@ import './socialButton.scss'
 export default (props) => (
     <a href={props.href} target="_blank" className={`button ${(props.fabIcon)}`}>
         <span className="icon"><FontAwesomeIcon icon={['fab', props.fabIcon]} size="1x" /></span>
-        <span>{props.content}</span>
+        <span>{props.children}</span>
     </a>
 );
--- src/layouts/contact/contact.js
+++ src/layouts/contact/contact.js
@@ -6,17 +6,9 @@ export default () => (
     <div data-aos="fade-up" className="container">
       <h1 data-aos="zoom-in" className="title">Let's get social</h1>
       <div className="buttons is-centered">
-        {/* <a href="https://www.linkedin.com/in/danielfr90/" target="_blank" className="button is-linkedin">
-          <span className="icon"><FontAwesomeIcon icon={['fab', 'linkedin-in']} size="1x" /></span>
-          <span>Linkedin</span>
-        </a>
-        <a href="https://www.linkedin.com/in/danielfr90/" target="_blank" className="button is-github">
-          <span className="icon"><FontAwesomeIcon icon={['fab', 'github']} size="1x" /></span>
-          <span>Github</span>
-        </a> */}
-        <SocialButton content={'twitter'} fabIcon={'twitter'} href={'https://twitter.com/chicolisto90'}></SocialButton>
-        <SocialButton content={'linkedin'} fabIcon={'linkedin-in'} href={'https://www.linkedin.com/in/danielfr90/'}></SocialButton>
-        <SocialButton content={'github'} fabIcon={'github'} href={'https://github.com/boxton90'}></SocialButton>
+        <SocialButton fabIcon="twitter" href="https://twitter.com/chicolisto90">twitter</SocialButton>
+        <SocialButton fabIcon="linkedin-in" href="https://www.linkedin.com/in/danielfr90/">linkedin</SocialButton>
+        <SocialButton fabIcon="github" href="https://github.com/boxton90">github</SocialButton>
       </div>
     </div>
   </section>
