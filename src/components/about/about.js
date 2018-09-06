4d0d39147f095f52a8c2ebdfbae3f584d48e4933
boxton90 <daniel.rabal@dnata.com>
2018-09-06 16:22:56 +0400

    About content query - Contentful link cleaner package addition

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,21 +1,32 @@
 import React from 'react'
 import avatar from "../../assets/avatar.jpg"
+import { StaticQuery, graphql } from "gatsby"
 
 export default () => (
-  <section id="about" className="section has-text-centered">
-    <div data-aos="fade-up" className="container">
-      <h1 data-aos="zoom-in" className="title">About me</h1>
-      <figure className="image avatar is-128x128">
-        <img className="is-rounded" src={avatar} alt="avatar"></img>
-      </figure>
-      <p>
-        American bobtail tiger but bengal sphynx egyptian mau or burmese lion.
-        Devonshire rex. Puma ragdoll, and mouser yet panther bombay yet malkin for panther.
-        Kitten. Kitten norwegian forest for tom for grimalkin so puma so siberian.
-        Russian blue russian blue scottish fold for kitten. Devonshire rex grimalkin. Cheetah kitten.
-        Jaguar tom for russian blue lynx. Puma cornish rex so egyptian mau and jaguar so mouser.
-        Havana brown thai for devonshire rex birman yet bobcat or balinese.
-      </p>
-    </div>
-  </section>
+  <StaticQuery
+    query={graphql`
+    query AboutQuery {
+      contentfulAbout{
+        title
+        desc{
+          internal{
+            content
+          }
+        }
+      }
+    }
+  `}
+    render={data => (
+      <section id="about" className="section has-text-centered">
+        <div data-aos="fade-up" className="container">
+          <h1 data-aos="zoom-in" className="title">{data.contentfulAbout.title}</h1>
+          <figure className="image avatar is-128x128">
+            <img className="is-rounded" src={avatar} alt="avatar"></img>
+          </figure>
+          <p>{data.contentfulAbout.desc.internal.content}</p>
+        </div>
+      </section>
+    )}
+  />
 );
+
