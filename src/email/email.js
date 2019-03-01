a988a90aece964fa6bc11c336296e093980e35c4
Daniel Fernandez Rabal <boxton.da@gmail.com>
2019-03-01 17:30:12 +0100

    Enviroment variables changes

--- src/email/email.js
+++ src/email/email.js
@@ -1,9 +1,9 @@
 import * as emailjs from 'emailjs-com'
 
 export const config = {
-    serviceID: process.env.EMAILJS_SERVICE_ID,
-    templateID: process.env.EMAILJS_TEMPLATE_ID,
-    userID: process.env.EMAILJS_USER_ID,
+    serviceID: process.env.GATSBY_EMAILJS_SERVICE_ID,
+    templateID: process.env.GATSBY_EMAILJS_TEMPLATE_ID,
+    userID: process.env.GATSBY_EMAILJS_USER_ID,
 }
 
 export { emailjs }
